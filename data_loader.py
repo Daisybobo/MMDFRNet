f1072d43d010b1e44c5c235297b10e83e8c94146
Daisybobo <109801735+Daisybobo@users.noreply.github.com>
2025-08-09 15:43:07 +0800

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -4,22 +4,19 @@ from dataset_enhanced import MyDataset
 
 
 def get_data_loaders(root_dir, batch_size=16, val_split=0.2, num_workers=4):
-    """获取训练和验证数据加载器"""
-    # 加载数据集
+
     train_dataset = MyDataset(root_dir, train=True)
     val_dataset = MyDataset(root_dir, train=False)
 
-    # 数据集分割
+
     dataset_size = len(train_dataset)
     indices = list(range(dataset_size))
     split = int((1 - val_split) * dataset_size)
     train_indices, val_indices = indices[:split], indices[split:]
 
-    # 创建采样器
     train_sampler = SubsetRandomSampler(train_indices)
     val_sampler = SubsetRandomSampler(val_indices)
 
-    # 创建数据加载器
     train_loader = DataLoader(
         train_dataset,
         batch_size=batch_size,
@@ -33,5 +30,6 @@ def get_data_loaders(root_dir, batch_size=16, val_split=0.2, num_workers=4):
         sampler=val_sampler,
         num_workers=num_workers
     )
+
 
     return train_loader, val_loader
